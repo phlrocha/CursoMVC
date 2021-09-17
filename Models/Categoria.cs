8b6f94ce3f3327e9f6d3a1b2694469e14b1a1a7c
Pedro Henrique Lima da Rocha <phrocha1@outlook.com>
2021-09-17 18:18:45 -0300

    Impementação CRUD Categoria e Produtos

---
+++ Models/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Descrição")]
+        [Required(ErrorMessage = "O campo Descrição é obrigatório!")]
+        public string Descricao { get; set; }
+
+        public List<Produto> Produtos { get; set; }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        [Range(1,100,ErrorMessage ="Selecionar um valor entre 1 e 100")]
+        public int Quantidade { get; set; }
+
+        public int CategoriaId { get; set; }
+
+        public Categoria Categoria { get; set; }
+
+    }
+}
